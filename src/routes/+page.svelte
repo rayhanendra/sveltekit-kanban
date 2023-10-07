6d1d0757930bcfe555e59cb9f15d56c4534283c6
Rayhanendra <mrayhann@gmail.com>
2023-10-07 15:55:49 +0700

    configure tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="tw-mx-auto tw-flex tw-flex-col tw-w-fit tw-border tw-p-4 tw-mt-4 tw-shadow-lg">
+	<h1 class="tw-text-5xl tw-text-sky-400">Welcome to SvelteKit</h1>
+	<h2 class="tw-text-2xl tw-text-gray-400 tw-font-light tw-mt-2 tw-mb-2">
+		Bootstrapped with TailwindCSS
+	</h2>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	prefix: 'tw-',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
